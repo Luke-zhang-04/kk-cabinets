ff98d7586d9d680818c30caca4e4efce1af764af
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-16 11:44:38 -0400

    added css and js for navbar fades

---
+++ docs/js/script.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+    scroll()
+}
+
+// Get the navbar
+let navbar = document.getElementById("navbar")
+
+// Add a background to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scroll() {
+    if (window.pageYOffset >= 100) {
+        navbar.classList.remove("background-default")
+        navbar.classList.remove("background-trans")
+        navbar.classList.add("background-white")
+    } else {
+        navbar.classList.remove("background-white")
+        navbar.classList.add("background-trans")
+    }
+}
