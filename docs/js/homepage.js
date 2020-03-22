0fd1206feacaa5cac49f651c1418719a11fa303e
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-22 14:12:01 -0400

    buttons link

--- docs/js/homepage.js
+++ docs/js/homepage.js
@@ -1,7 +1,8 @@
 let timelines = []
 let scenes = []
 const controller = new ScrollMagic.Controller()
-let alternating = 0;
+let alternating = 0
+let multiplyer 
 
 let displayedTestimonial
 let para = document.getElementById("testimonial")
@@ -19,45 +20,25 @@ function getTestimonials(obj) {
 
 if ($( window ).width() >= 767) {
     $(".content").each(function() {
-        if (alternating % 2 == 0) {
-            let tl = new TimelineMax();
-            timelines.push(tl)
-            tl.from($(this).find("h5 article"), .5, {x:200, opacity: 0})
-            tl.from($(this).find("h5 span"), 1, { width: 0}, "=-.5")
-            tl.from($(this).find("h5 p"), 1, {x:200, opacity: 0}, "=-.75")
-            tl.from($(this).find("h5 button"), 1, {x:200, opacity: 0}, "-.25")
-            tl.from($(this).find(".ci1"), 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1")
-            tl.from($(this).find(".ci2"), 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7")
-            const scene = new ScrollMagic.Scene({
-                triggerElement: this,
-                triggerHook: "onLeave",
-                duration: "250%"
-            })
-            .setPin(this)
-            .setTween(tl)
-            .addTo(controller)
+        multiplyer = (alternating % 2 == 0 ? 1 : -1)
+        let tl = new TimelineMax();
+        timelines.push(tl)
+        tl.from($(this).find("h5 article"), .5, {x:200*multiplyer, opacity: 0})
+        tl.from($(this).find("h5 span"), 1, { width: 0}, "=-.5")
+        tl.from($(this).find("h5 p"), 1, {x:200*multiplyer, opacity: 0}, "=-.75")
+        tl.from($(this).find("h5 .btn"), 1, {x:200*multiplyer, opacity: 0}, "-.25")
+        tl.from($(this).find(".ci1"), 1, {x:-200*multiplyer, opacity: 0,ease: Power4.easeInOut}, "=-1")
+        tl.from($(this).find(".ci2"), 1, {x:200*multiplyer, opacity: 0, ease: Power4.easeInOut}, "=-.7")
+        const scene = new ScrollMagic.Scene({
+            triggerElement: this,
+            triggerHook: "onLeave",
+            duration: "250%"
+        })
+        .setPin(this)
+        .setTween(tl)
+        .addTo(controller)
 
-            scenes.push(scene)
-        } else {
-            let tl = new TimelineMax();
-            timelines.push(tl)
-            tl.from($(this).find("h5 article"), .5, {x:-200, opacity: 0})
-            tl.from($(this).find("h5 span"), 1, { width: 0}, "=-.5")
-            tl.from($(this).find("h5 p"), 1, {x:-200, opacity: 0}, "=-.75")
-            tl.from($(this).find("h5 button"), 1, {x:-200, opacity: 0}, "-.25")
-            tl.from($(this).find(".ci1"), 1, {x:200, opacity: 0,ease: Power4.easeInOut}, "=-1")
-            tl.from($(this).find(".ci2"), 1, {x:-200, opacity: 0, ease: Power4.easeInOut}, "=-.7")
-            const scene = new ScrollMagic.Scene({
-                triggerElement: this,
-                triggerHook: "onLeave",
-                duration: "250%"
-            })
-            .setPin(this)
-            .setTween(tl)
-            .addTo(controller)
-
-            scenes.push(scene)
-        }
+        scenes.push(scene)
         alternating++
     })
 }
