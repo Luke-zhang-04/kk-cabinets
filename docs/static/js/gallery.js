e7367b3597729234164938d6d80702cb0e685620
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-03-25 11:25:28 -0400

    added filter prototypes

--- docs/static/js/gallery.js
+++ docs/static/js/gallery.js
@@ -15,6 +15,16 @@ let filterOptions = {
     locations: []
 }
 
+//active filters
+let activeFilters = {
+    colours: [],
+    materials: [],
+    cabinets: null,
+    countertop: null,
+    pattern: null,
+    locations: []
+}
+
 //if array conains target
 function contains(target, array) {
     for (i of array) {
@@ -23,6 +33,13 @@ function contains(target, array) {
         }
     }
     return false
+}
+
+//remove element from array
+function arrayRemove(arr, value) {
+    return arr.filter(function(ele){
+        return ele != value
+    })
 }
 
 //get projects from firestore
@@ -34,23 +51,36 @@ db.collection("gallery").get().then((snapshot) => {
         //get all possible filter options
         let details = doc.data().details
         for (infoType of infoTypes) {
+            //if filter category not included yet, push it
             if (!(contains(details[infoType], filterOptions[infoType+"s"]))) {
                 filterOptions[infoType+"s"].push(details[infoType])
             }
         }
     })
-    for ([count, infoType] of infoTypes.entries()) {
-        for (info of filterOptions[infoType+"s"]) {
-            $("#filter" + count).append(
-                "<button>" + info + "</button>"
+
+    //create buttons for filtering
+    for ([count, infoType] of infoTypes.entries()) { //iterate through each filter type
+        for (info of filterOptions[infoType+"s"]) { //within these types, get all filter options
+            let id = info + "_" + infoType //unique id for each button
+            $("#filter" + count).append( //create button for seleting filter
+                "<button id=" + id + ">" + info + "</button>"
             )
+            let filter_name = info
+            let filter_type = infoType
+            $("#"+id).click(function() { //create event listener for
+                if (!contains(filter_name, activeFilters[filter_type+"s"])) {
+                    activeFilters[filter_type+"s"].push(filter_name)
+                } else {
+                    activeFilters[filter_type+"s"] = arrayRemove(activeFilters[filter_type+"s"], filter_name)
+                }
+            })
         }
     }
-    display_batch()
+    display_batch(data)
 })
 
 //handle data and display images accordingly
-function display_batch() {
+function display_batch(data) {
     for (key in data) {
         imgURL = storageRef.child(data[key]["file"]) //image url
         let column = columns[columnNum%4] //column to append image to
@@ -65,7 +95,7 @@ function display_batch() {
             let element = $("#" + id) //shortcut for target element
 
             //variable for information
-            let info = "Colour: " + data[id]["details"]["colour"].charAt(0).toUpperCase() + data[id]["details"]["colour"].slice(1) + "<br/>Furniture: "
+            let info = "<br/>Colour: " + data[id]["details"]["colour"].charAt(0).toUpperCase() + data[id]["details"]["colour"].slice(1) + "<br/>Furniture: "
 
             //furniture type
             if (data[id]["details"]["furniture"]["countertop"] && data[id]["details"]["furniture"]["cabinet"]) {
@@ -91,9 +121,8 @@ function display_batch() {
         columnNum++
     }
     //remove loading gif
-    $("#loading").remove()
+    $("#loading").css("display", "none")
 }
-
 //makes information drop down
 function expand(key) {
     let element = document.getElementById(key)
@@ -105,6 +134,7 @@ function expand(key) {
     }
 }
 
+//expand filter dropdown
 function expand_filter(numb) {
     let element = document.getElementById("filter"+numb)
     if (element.style.maxHeight){
@@ -112,4 +142,29 @@ function expand_filter(numb) {
     } else {
         element.style.maxHeight = element.scrollHeight + "px";
     }
+}
+
+function apply_filters() {
+    const filterTypes = ["colour", "material", "location"]
+    console.log(activeFilters)
+    $(".image_container").remove()
+    $("#loading").css("display", "block")
+    let filteredData = new Map
+    console.log(data, "DATA")
+    for (key in data) {
+        doc = data[key]
+        let broken = false
+        for ([_, filter] of filterTypes.entries()) {
+            if (contains(doc["details"][filter], activeFilters[filter+"s"])) {
+                //console.log(doc, filter)
+                //filteredData[key] = doc
+                broken = true
+                break
+            }
+        }
+        if (!broken) {
+            filteredData[key] = doc
+        }
+    }
+    display_batch(filteredData)
 }
