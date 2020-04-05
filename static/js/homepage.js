bfac7b9d385bdba9bc51b316e6f9c26c1da8dc6a
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-04-05 12:42:41 -0400

    added scroll down button

--- static/js/homepage.js
+++ static/js/homepage.js
@@ -42,3 +42,23 @@ if ($( window ).width() >= 767) {
         alternating++
     })
 }
+
+$("#downBtn").on("click", function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "" && this.hash) {
+        // Prevent default anchor click behavior
+        event.preventDefault()
+
+        // Store hash
+        var hash = this.hash
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - $(window).height()
+        }, 800, function(){
+ 
+            // Add hash (#) to URL when done scrolling (default click behavior)
+        })
+    } // End if
+})
