85158ae7256c9ded901d02be5dff6ee6cd5c2934
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-01-25 20:39:30 -0500

    refactor testimonials
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/_firebase/index.ts
@@ -0,0 +1,31 @@
+/**
+ * KK Cabinets
+ * @copyright 2020 Luke Zhang, Ethan Lim
+ * @author luke zhang, Ethan Lim
+ *
+ * https://luke-zhang-04.github.io
+ * https://github.com/ethanlim04
+ *
+ * @license GPL-3.0
+ */
+
+declare type Firebase = typeof import("firebase/app").default
+declare const firebase: Firebase
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8LzSIS1mRoQuFnjtwq1U5vtp2Hx2rdtk",
+    authDomain: "kk-cabinets.firebaseapp.com",
+    databaseURL: "https://kk-cabinets.firebaseio.com",
+    projectId: "kk-cabinets",
+    storageBucket: "kk-cabinets.appspot.com",
+    messagingSenderId: "999203573812",
+    appId: "1:999203573812:web:7baa8753bce4e8ac187a61"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export const firestore = firebase.firestore?.()
+
+export default {
+    firestore,
+}
---
+++ src/testimonials/index.tsx
@@ -0,0 +1,64 @@
+/**
+ * KK Cabinets
+ * @copyright 2020 Luke Zhang, Ethan Lim
+ * @author luke zhang, Ethan Lim
+ *
+ * https://luke-zhang-04.github.io
+ * https://github.com/ethanlim04
+ *
+ * @license GPL-3.0
+ */
+
+import * as DeStagnate from "destagnate/lib/createElementOnly"
+import type firebase from "firebase"
+import {firestore as db} from "../_firebase"
+
+type Testimonials = {[key: number]: string}
+
+const isTestimonials = (
+        obj: firebase.firestore.DocumentData
+    ): obj is Testimonials => typeof obj === "object",
+
+    handleScroll = (): void => {
+        const elements = Array.from(
+            document.getElementsByClassName("section") as HTMLCollectionOf<HTMLElement>
+        )
+
+        for (const element of elements) {
+            if (
+                window.pageYOffset + window.screen.height * 0.8 >= element.offsetTop &&
+                !element.classList.contains("raise")
+            ) {
+                element.classList.add("opaque") // Make text appear
+                element.classList.add("raise")
+                element.classList.remove("transparent") // Remove transparent class
+            } else if (
+                window.pageYOffset + window.screen.height * 0.85 < element.offsetTop &&
+                element.classList.contains("raise")
+            ) {
+                element.classList.remove("opaque") // Make text appear
+                element.classList.remove("raise")
+                element.classList.add("transparent") // Remove transparent class
+            }
+        }
+    };
+
+(async (): Promise<void> => {
+    const data = (await db.collection("testimonials").get()).docs[0].data(),
+        container = document.getElementById("testimonial_container")
+
+    if (isTestimonials(data) && container) {
+        for (const [index, testimonial] of Object.values(data).entries()) {
+            console.log({testimonial, data})
+            container.appendChild(<div class="container section transparent testimonial">
+                <div class={`jumbotron-${index % 2 === 0 ? "1" : "2"}`}>
+                    <p>{`"${testimonial}"`}</p>
+                </div>
+            </div>)
+        }
+    }
+
+    handleScroll()
+})()
+
+window.addEventListener("scroll", handleScroll)
