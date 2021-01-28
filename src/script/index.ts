dfcced29d88553d74d9499bcbfcd1050fd0f37f6
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-01-27 19:41:19 -0500

    update navbar script
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/script/index.ts
@@ -0,0 +1,32 @@
+/**
+ * KK Cabinets
+ * @copyright 2020 - 2021 Luke Zhang, Ethan Lim
+ * @author luke zhang, Ethan Lim
+ *
+ * https://luke-zhang-04.github.io
+ * https://github.com/ethanlim04
+ *
+ * @license GPL-3.0-or-later
+ */
+
+// Get the navbar
+const navbar = document.getElementById("navbar"),
+
+    /**
+     * Add a background to the navbar when you reach its scroll position.
+     * Remove "sticky" when you leave the scroll position
+     */
+    scroll = () => {
+        if (window.pageYOffset >= 100) {
+            navbar?.classList.remove("background-default")
+            navbar?.classList.remove("background-trans")
+            navbar?.classList.add("background-white")
+        } else {
+            navbar?.classList.remove("background-white")
+            navbar?.classList.add("background-trans")
+        }
+    }
+
+window.addEventListener("scroll", scroll)
+
+export {}
