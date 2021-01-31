4d53ddc68dae4b493f4e6308276e2e04c1daddee
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2021-01-31 10:13:40 -0500

    refactor gallery
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

---
+++ src/gallery/clickListeners.ts
@@ -0,0 +1,129 @@
+/**
+ * KK Cabinets
+ * @copyright 2020 - 2021 Luke Zhang, Ethan Lim
+ * @author luke zhang, Ethan Lim
+ *
+ * https://luke-zhang-04.github.io
+ * https://github.com/ethanlim04
+ *
+ * @license GPL-3.0-or-later
+ */
+
+import "regenerator-runtime/runtime"
+
+
+// Filter Expanding
+document.querySelectorAll<HTMLElement>(".expand-filter").forEach((element, index) => {
+    element.addEventListener("click", () => {
+        const filter = document.querySelector(`#filter${index}`)
+
+        if (filter && filter instanceof HTMLElement) {
+            if (filter.style.maxHeight) {
+                filter.style.maxHeight = ""
+            } else {
+                filter.style.maxHeight = `${filter.scrollHeight}px`
+            }
+        }
+    })
+})
+
+// Toggler for pattern fitler
+document.querySelector<HTMLElement>("#pattern_toggle")?.addEventListener("click", async (event) => {
+    const {gallery} = await import("./index")
+
+    if (event.target instanceof HTMLElement) {
+        const icon = event.target.querySelector("span")
+
+        if (icon?.innerText === "remove") { // If current setting is either or
+            icon.innerText = "clear"
+            gallery.setState({activeFilters: {
+                ...gallery.getState.activeFilters,
+                pattern: false,
+            }})
+        } else if (icon?.innerText === "clear") { // If current setting is no pattern
+            icon.innerText = "done"
+            gallery.setState({activeFilters: {
+                ...gallery.getState.activeFilters,
+                pattern: true,
+            }})
+        } else if (icon?.innerText === "done") { // If current setting is with a pattern
+            icon.innerText = "remove"
+            gallery.setState({activeFilters: {
+                ...gallery.getState.activeFilters,
+                pattern: undefined,
+            }})
+        }
+
+        gallery.applyFilters()
+    }
+})
+
+// Toggler for furniture types
+document.querySelectorAll<HTMLElement>(".furniture_dropdown").forEach((element) => {
+    const furnitureDropdowns = ["either", "both", "countertop", "cabinets"]
+
+    element.addEventListener("click", async (event) => {
+        if (event.target instanceof HTMLElement) {
+            const {gallery} = await import("./index")
+
+            for (const dropdown of furnitureDropdowns) {
+                const dropdownBtn = document.querySelector<HTMLElement>(`#${dropdown}_furniture span`)
+
+                if (dropdownBtn) {
+                    dropdownBtn.innerText = ""
+                }
+            }
+
+
+            const icon = event.target.querySelector<HTMLElement>("span"),
+                filterChoice = event.target.id.replace("_furniture", "")
+
+            if (icon) {
+                icon.innerHTML = "done"
+            }
+
+            switch(filterChoice) {
+                case "both": // If current setting is both countertops and cabinets
+                    gallery.setState({activeFilters: {
+                        ...gallery.getState.activeFilters,
+                        cabinets: true,
+                        countertop: true,
+                    }})
+                    break
+
+                case "either": // If current setting is either countertops or cabinets
+                    gallery.setState({activeFilters: {
+                        ...gallery.getState.activeFilters,
+                        cabinets: undefined,
+                        countertop: undefined,
+                    }})
+                    break
+
+                case "countertop": // If current setting is countertops only
+                    gallery.setState({activeFilters: {
+                        ...gallery.getState.activeFilters,
+                        cabinets: false,
+                        countertop: true,
+                    }})
+                    break
+
+                case "cabinets": // If current setting is cabinets only
+                    gallery.setState({activeFilters: {
+                        ...gallery.getState.activeFilters,
+                        cabinets: true,
+                        countertop: false,
+                    }})
+                    break
+            }
+
+            gallery.applyFilters()
+        }
+    })
+})
+
+document.querySelector<HTMLElement>("#clear-filters")?.addEventListener("click", async () => (
+    (await import("./index")).gallery.clearFilters()
+))
+document.querySelector<HTMLElement>("#apply-filters")?.addEventListener("click", async () => (
+    (await import("./index")).gallery.applyFilters()
+))
---
+++ src/gallery/utils.ts
@@ -0,0 +1,18 @@
+/**
+ * Splits an array into chunks
+ * @param arr - array to split
+ * @param chunkSize - size of array chunks
+ */
+export const arrayToChunks = <T extends unknown>(arr: T[], chunkSize = 3): T[][] => {
+    const chunks: T[][] = []
+
+    for (let index = 0; index < arr.length; index += chunkSize) {
+        chunks.push(arr.slice(index, index + chunkSize))
+    }
+
+    return chunks
+}
+
+export default {
+    arrayToChunks,
+}
